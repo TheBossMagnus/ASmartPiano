b4ae3d2a4d0a262c9028ced538402614b54acd31
TheBossMagnus <TheBossMagnus@proton.me>
2024-05-24 09:00:04 +0200

    fix sfarfallio display suona
    Signed-off-by: TheBossMagnus <TheBossMagnus@proton.me>

--- Suona.ino
+++ Suona.ino
@@ -1,48 +1,54 @@
 void suona() {
   lcd.clear();
   lcd.print("Suona liberamente!");
+  String nota_prec = "";
+
+
   while (digitalRead(BUTTON_PIN_MI)==HIGH){ //il mi per selezionare suona non deve essere una nota
   }
   
-  while (digitalRead(BUTTON_PIN_MI)!=HIGH) {
+  while (true) {
     if (digitalRead(BUTTON_PIN_RESET) == HIGH) {
       lcd.clear();
       return;
     }
 
-    int frequency = 0;
-    String note = "";
+    int frequenza = 0;
+    String nota = "";
 
     if (digitalRead(BUTTON_PIN_DO) == HIGH) {
-      note = "DO";
-      frequency = 262;
+      nota = "DO";
+      frequenza = 262;
     } else if (digitalRead(BUTTON_PIN_RE) == HIGH) {
-      note = "RE";
-      frequency = 294;
+      nota = "RE";
+      frequenza = 294;
     } else if (digitalRead(BUTTON_PIN_MI) == HIGH) {
-      note = "MI";
-      frequency = 330;
+      nota = "MI";
+      frequenza = 330;
     } else if (digitalRead(BUTTON_PIN_FA) == HIGH) {
-      note = "FA";
-      frequency = 349;
+      nota = "FA";
+      frequenza = 349;
     } else if (digitalRead(BUTTON_PIN_SOL) == HIGH) {
-      note = "SOL";
-      frequency = 392;
+      nota = "SOL";
+      frequenza = 392;
     } else if (digitalRead(BUTTON_PIN_LA) == HIGH) {
-      note = "LA";
-      frequency = 440;
+      nota = "LA";
+      frequenza = 440;
     } else if (digitalRead(BUTTON_PIN_SI) == HIGH) {
-      note = "SI";
-      frequency = 494;
+      nota = "SI";
+      frequenza = 494;
     } else if (digitalRead(BUTTON_PIN_DOA) == HIGH) {
-      note = "DO+";
-      frequency = 523;
+      nota = "DO+";
+      frequenza = 523;
     }
 
-    if (frequency > 0) {
-      lcd.clear();
-      lcd.print(note);
-      tone(BUZZER_PIN, frequency, 100);
+    if (frequenza > 0) {
+      tone(BUZZER_PIN, frequenza, 100);
+      if (nota_prec != nota) {
+        lcd.clear();
+        lcd.print(nota);
+        nota_prec = nota;
+      }
     } else {
       noTone(BUZZER_PIN);
     }
